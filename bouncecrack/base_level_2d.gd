e37f98e20b5ee7eb9e22a59b828e8370a8595cc2
Andrew Mattson <acmattson3@alaska.edu>
2024-09-23 11:51:52 -0800

    Initial commit with Godot project and base_level scene.

---
+++ bouncecrack/base_level_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name BaseLevel
+# The base level scene for all future levels to inheret from
+# Includes any core functionalities common to all forseeable levels
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
