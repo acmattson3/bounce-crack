05020fddfad8da5382d55f78ccf3419cba37f6f2
Andrew Mattson <acmattson3@alaska.edu>
2024-09-25 15:08:44 -0800

    Added ball (uses areas for bounce logic). Updated ball and paddle speeds to variables instead of constants for future fun.

---
+++ bouncecrack/ball/ball_2d.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 1200.0
+var direction := Vector2.DOWN # Start by going straight down
+
+func _physics_process(delta: float) -> void:
+	velocity = direction * speed
+	move_and_slide()
+
+func _on_bottom_area_2d_body_entered(body: Node2D) -> void:
+	if body is Paddle:
+		direction = ($NewBounceComparePos.global_position - body.global_position).normalized()
+	else:
+		direction.y = -direction.y
+func _on_top_area_2d_body_entered(body: Node2D) -> void:
+	direction.y = -direction.y
+func _on_right_area_2d_body_entered(body: Node2D) -> void:
+	direction.x = -direction.x
+func _on_left_area_2d_body_entered(body: Node2D) -> void:
+	direction.x = -direction.x
--- bouncecrack/player_paddle/player_paddle_2d.gd
+++ bouncecrack/player_paddle/player_paddle_2d.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
+class_name Paddle
 
-
-const SPEED = 600.0
+var speed = 600.0
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("paddle_left", "paddle_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
