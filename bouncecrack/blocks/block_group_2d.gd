32e87913a2c9fd5ef62314c7c5ae65d59aff0fb3
Andrew Mattson <acmattson3@alaska.edu>
2024-10-01 12:48:35 -0800

    Simplified block group to work well with the editor (changes to exported variables are immediately visible).

--- bouncecrack/blocks/block_group_2d.gd
+++ bouncecrack/blocks/block_group_2d.gd
@@ -6,15 +6,28 @@ extends Node2D
 var block_scene := load("res://blocks/breakable_block.tscn")
 const block_dims := Vector2(194.0, 80.0)
 
-@export var group_size_x: int = 4
-@export var group_size_y: int = 4
-@export var spacing: float = 10.0
+@export var group_size_x: int = 4:
+	set(value):
+		group_size_x = value
+		_set_block_group()
+@export var group_size_y: int = 4:
+	set(value):
+		group_size_y = value
+		_set_block_group()
+@export var spacing: float = 10.0:
+	set(value):
+		spacing = value
+		_set_block_group()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_set_block_group()
+
+func _set_block_group():
 	var total_size := Vector2(group_size_x*(block_dims.x + spacing) - spacing, group_size_y*(block_dims.y + spacing) - spacing)
-	# Box shows what the size of the group of blocks will be!
-	$GroupArea2D/CollisionShape2D.shape.size = total_size
+	
+	for block in get_children():
+		block.queue_free()
 	
 	for i in range(group_size_y):
 		var y_pos := 0.5*(block_dims.y-total_size.y) + i*(block_dims.y+spacing)
