cdf419372b44695a69bdc460f97fc5789d0cbe7b
Andrew Mattson <acmattson3@alaska.edu>
2024-10-12 18:31:22 -0800

    Fixed ball so that glancing hits are ignored.

--- bouncecrack/ball/ball_2d.gd
+++ bouncecrack/ball/ball_2d.gd
@@ -24,6 +24,8 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _on_bottom_area_2d_body_entered(body: Node2D) -> void:
+	if direction.y < 0:
+		return
 	if abs(direction.y) < 0.05:
 		direction.x = -direction.x
 	if body is Paddle:
@@ -36,17 +38,19 @@ func _on_bottom_area_2d_body_entered(body: Node2D) -> void:
 func _on_top_area_2d_body_entered(body: Node2D) -> void:
 	if body is Paddle:
 		return
+	if direction.y > 0:
+		return
 	direction.y = -direction.y
 	if body is BreakableBlock:
 		body.break_block()
 func _on_right_area_2d_body_entered(body: Node2D) -> void:
-	if body is Paddle:
+	if direction.x < 0:
 		return
 	direction.x = -direction.x
 	if body is BreakableBlock:
 		body.break_block()
 func _on_left_area_2d_body_entered(body: Node2D) -> void:
-	if body is Paddle:
+	if direction.x > 0:
 		return
 	if abs(direction.x) < 0.05:
 		direction.y = -direction.y
