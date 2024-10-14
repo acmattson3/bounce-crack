44522dded3cc87b351efda5e36c22aec7a3f909d
Andrew Mattson <acmattson3@alaska.edu>
2024-10-13 18:00:00 -0800

    Updated power up speed to be variable for future extensibility.

--- bouncecrack/power_up/power_up.gd
+++ bouncecrack/power_up/power_up.gd
@@ -1,14 +1,11 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-var out_of_bounds : bool = false;
+var out_of_bounds : bool = false
+var speed: float = 700.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += 700.0 * delta
+	position.y += speed * delta # Fall speed pixels/second
 	
 	if global_position.y >= 1000.0 and not out_of_bounds:
 		out_of_bounds = true
