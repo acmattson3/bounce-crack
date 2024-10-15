888be2f2fdb9ab6dfd38a69a4ae5a3555bc7455e
Andrew Mattson <acmattson3@alaska.edu>
2024-10-15 12:24:22 -0800

    Updated pause screen to update level name dynamically in editor via exported name.

--- bouncecrack/pause/pause.gd
+++ bouncecrack/pause/pause.gd
@@ -1,15 +1,22 @@
+@tool
 extends PanelContainer
 
-@export var level_name: String = "Unnamed level"
+@export var level_name: String = "Unnamed level":
+	set(value):
+		%LevelNameLabel.text = value
+		level_name = value
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	%LevelNameLabel.text = level_name
+	if Engine.is_editor_hint():
+		return # Allows us to use @tool without breaking the editor
 	process_mode = PROCESS_MODE_ALWAYS # We are unaffected by pausing the scene tree.
 	get_tree().paused = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return # Allows us to use @tool without breaking the editor
 	if Input.is_action_just_pressed("space") or Input.is_action_just_pressed("escape") and visible:
 		get_tree().paused = false
 		hide()
